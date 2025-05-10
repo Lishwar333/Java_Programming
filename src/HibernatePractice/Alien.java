156edeec3a6c45bc0f9e7fee31d3b52d98caf325
LishwarK <jayaklish@gmail.com>
2025-05-10 19:43:39 +0200

    Java to maven project

---
+++ src/HibernatePractice/Alien.java
@@ -0,0 +1,39 @@
+package HibernatePractice;
+
+public class Alien {
+	
+	public int aid;
+	public String aname;
+	public String color;
+	
+	public Alien(int aid, String name, String color) {
+		
+		this.aid = aid;
+		this.aname = aname;
+		this.color = color;
+	}
+
+	public int getAid() {
+		return aid;
+	}
+
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+
+	public String getAname() {
+		return aname;
+	}
+
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
---
+++ src/HibernatePractice/App.java
@@ -0,0 +1,13 @@
+package HibernatePractice;
+
+public class App {
+	
+	public static void main(String args[]) {
+		
+		Alien alien1 = new Alien(1,"a1","g");
+		
+		System.out.println(alien1.aid);
+		
+	}
+
+}
